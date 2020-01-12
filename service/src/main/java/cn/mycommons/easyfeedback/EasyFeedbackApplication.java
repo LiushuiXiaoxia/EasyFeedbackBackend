152fcd161ea9f5d445a2a999b62d98fdbaa153b1
xiaqiulei <xiaqiulei@126.com>
2020-01-12 22:12:29 +0800

    init

---
+++ service/src/main/java/cn/mycommons/easyfeedback/EasyFeedbackApplication.java
@@ -0,0 +1,12 @@
+package cn.mycommons.easyfeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyFeedbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyFeedbackApplication.class, args);
+    }
+}
---
+++ service/src/test/java/cn/mycommons/easyfeedback/EasyFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.mycommons.easyfeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyFeedbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
